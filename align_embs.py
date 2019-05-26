31001ff5dfa8331b89bfaa11779331789208c4f7
YY Ahn <yongyeol@gmail.com>
2019-05-26 10:05:19 -0400

    reusing the two-embedding alignment function

--- align_embs.py
+++ align_embs.py
@@ -1,6 +1,3 @@
-import numpy as np
-
-
 """
     Example of embedding's data structure
     embs = {
@@ -11,14 +8,18 @@ import numpy as np
 
 """
 
-def align_two_embs(emb_to_align, emb_base):
+import numpy as np
+
+
+def align_two_embs(emb_to_align, emb_base, common_keys=None):
     """
     :param emb_to_align: embedding vectors to be align
     :param emb_base: base embedding vectors
     :return:
         aligned_embeddings of emb_to_align
     """
-    common_keys = list(set(emb_to_align.keys()).intersection(set(emb_base.keys())))
+    if not common_keys:
+        common_keys = list(set(emb_to_align.keys()).intersection(set(emb_base.keys())))
 
     A = np.array([emb_to_align[key] for key in common_keys]).T
     B = np.array([emb_base[key] for key in common_keys]).T
@@ -35,19 +36,14 @@ def align_list_of_embs(emb_list, emb_base):
     :param emb_list: list of embedding vectors to be align
     :param emb_base: base embedding vectors
     :return:
-        list of aligned_embeddings 
+        list of aligned_embeddings
     """
     common_keys = set.intersection(*[set(emb.keys()) for emb in emb_list])
     common_keys = list(common_keys.intersection(set(emb_base.keys())))
-    
+
     aligned_embeddings = []
     for emb_to_align in emb_list:
-        A = np.array([emb_to_align[key] for key in common_keys]).T
-        B = np.array([emb_base[key] for key in common_keys]).T
-        M = B.dot(A.T)
-        u, sigma, v_t = np.linalg.svd(M)
-        rotation_matrix = u.dot(v_t)
-        aligned_embedding = {k: rotation_matrix.dot(v) for k, v in emb_to_align.items()}
-        aligned_embeddings.append(aligned_embedding)
-        
+        aligned_emb = align_two_embs(emb_to_align, emb_base, common_keys)
+        aligned_embeddings.append(aligned_emb)
+
     return aligned_embeddings
