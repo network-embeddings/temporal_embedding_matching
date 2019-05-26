2500e1e475ec5cdc1d618014373e4b180e06d598
balla2081 <balla2081@postech.ac.kr>
2019-05-25 21:24:19 -0400

    add_comments

--- align_embs.py
+++ align_embs.py
@@ -1,6 +1,23 @@
 import numpy as np
 
+
+"""
+    Example of embedding's data structure
+    embs = {
+        a: [0,0,1,....,0],
+        b: [1,0,0,....,0],
+        ...
+    }
+
+"""
+
 def align_two_embs(emb_to_align, emb_base):
+    """
+    :param emb_to_align: embedding vectors to be align
+    :param emb_base: base embedding vectors
+    :return:
+        aligned_embeddings of emb_to_align
+    """
     common_keys = list(set(emb_to_align.keys()).intersection(set(emb_base.keys())))
 
     A = np.array([emb_to_align[key] for key in common_keys]).T
@@ -13,7 +30,13 @@ def align_two_embs(emb_to_align, emb_base):
     return aligned_embedding
 
 
-def align_list_of_embeddings(emb_list, emb_base):
+def align_list_of_embs(emb_list, emb_base):
+    """
+    :param emb_to_align: list of embedding vectors to be align
+    :param emb_base: base embedding vectors
+    :return:
+        list of aligned_embeddings of emb_to_align
+    """
     common_keys = set.intersection(*[set(emb.keys()) for emb in emb_list])
     common_keys = list(common_keys.intersection(set(emb_base.keys())))
     
@@ -28,4 +51,3 @@ def align_list_of_embeddings(emb_list, emb_base):
         aligned_embeddings.append(aligned_embedding)
         
     return aligned_embeddings
-        
